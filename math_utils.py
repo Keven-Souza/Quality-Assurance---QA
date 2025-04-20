74321be63d0a6cc7038e31d5fda2b9e652323258
Keven-Souza <kevensouza5558jesus@gmail.com>
2025-04-20 20:56:26 -0300

    math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,1 +1,21 @@
+def add(a, b):
+
+ return a + b
+
+def subtract(a, b):
+
+ return a - b
+
+2. Crie um arquivo `test_math_utils.py` com os testes:
+
+from math_utils import add, subtract
+
+def test_add():
+
+ assert add(2, 3) == 5
+
+def test_subtract():
+
+ assert subtract(5, 2) == 3
+
 
